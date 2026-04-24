32b2fee4a01ab4c3fb463bb7a7805a5a1f89d569
maxconsul <maxconsul17@gmail.com>
2026-04-24 21:58:54 +0800

    update ip

--- client/src/components/Room.js
+++ client/src/components/Room.js
@@ -1,7 +1,7 @@
 import { useEffect, useRef, useState } from 'react';
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:5000');
+const socket = io('http://18.143.90.78:5000');
 
 function Room({ roomId, username }) {
   const [users, setUsers] = useState([]);
